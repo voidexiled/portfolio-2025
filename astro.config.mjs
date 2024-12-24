7f61abdc21825e76dad66f85ed67bf31428f7585
voidexiled <l20070518@cdmadero.tecnm.mx>
2024-12-24 03:00:48 -0600

    upload cv and links updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site: "https://jesusjalomo.vercel.app",
+  integrations: [react(), robotsTxt()],
 });
